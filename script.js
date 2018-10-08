acdb43648c0d4b06d0ac586d2f9977a08fa0b3de
wzoon <ouaalalou.hamza@gmail.com>
2018-10-08 23:54:44 +0100

    Seek and Destroy

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
   }
   
   console.log(sumAll([10, 5]));
-  */
+
  function diffArray(arr1, arr2) {
     // Same, same; but different.
     return arr1
@@ -19,3 +19,18 @@
   }
   
   console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+  */
+ 
+ function destroyer(arr) {
+    var args = Array.prototype.slice.call(arguments);
+  
+    for (var i = 0; i < arr.length; i++) {
+      for (var j = 0; j < args.length; j++) {
+        if (arr[i] === args[j]) {
+          delete arr[i];
+        }
+      }
+    }
+    return arr.filter(Boolean);
+  }
+ console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
