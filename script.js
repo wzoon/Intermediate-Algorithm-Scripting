9b319f59f435eaebc4a94b3d0f509a50e4cc67a3
wzoon <ouaalalou.hamza@gmail.com>
2018-10-08 23:51:32 +0100

    diff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function sumAll(arr) {
+/*function sumAll(arr) {
     arr.sort((a,b) => a -b);
     let sum = 0;
     for(let i=arr[0];i<=arr[1];i++){
@@ -8,3 +8,14 @@ function sumAll(arr) {
   }
   
   console.log(sumAll([10, 5]));
+  */
+ function diffArray(arr1, arr2) {
+    // Same, same; but different.
+    return arr1
+      .filter(elem => !arr2.includes(elem))
+      .concat(
+        arr2.filter(elem => !arr1.includes(elem))
+      )
+  }
+  
+  console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
